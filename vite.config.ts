0a6b0ae53abeb3b70dfc4f09355909fa83734394
jantoniofc <jantoniofc@gmail.com>
2026-03-11 22:10:28 -0300

    chore: fix linting and formatting issues identified in CI

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@
 
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
 import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	plugins: [
